2e505601165ff5a51977a37c67f8c2f1655ee816
Kimmy Monassar <kimmymonassar@gmail.com>
2024-06-03 22:28:54 +0200

    Added mq + rolling logs

--- dht22.py
+++ dht22.py
@@ -4,9 +4,25 @@ import logging.handlers
 import time
 import board
 import adafruit_dht
+import pika
 from datetime import datetime
 
 logs_path = f'{os.path.dirname(os.path.realpath(__file__))}/logs/output.log'
+current_humidity = None
+current_temp_c = None
+current_temp_f = None
+
+def queue_publish(temp_f, temp_c, humidity):
+    print((temp_f))
+    print((temp_c))
+    print(str(humidity))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=36000))
+    channel = connection.channel()
+    channel.queue_declare(queue='dht22temp', durable=True)
+    channel.basic_publish(exchange='dhtexchange', routing_key='current_temp_fahrenheit', body=str(temp_f))
+    channel.basic_publish(exchange='dhtexchange', routing_key='current_temp_celsius', body=str(temp_c))
+    channel.basic_publish(exchange='dhtexchange', routing_key='current_humidity', body=str(humidity))
+    connection.close()
 
 def rolling_log_setup():
     log_handler = logging.handlers.TimedRotatingFileHandler(logs_path, when='midnight', backupCount=31)
@@ -21,15 +37,15 @@ def tempcheck(x):
 
     for i in range(x):
         try:
-            temperature_c = dhtDevice.temperature
-            temperature_f = temperature_c * (9 / 5) + 32
-            humidity = dhtDevice.humidity
-            log_data = datetime.now().strftime('%d/%m/%Y %H:%M:%S - ') + 'Temp: {:.1f} F / {:.1f} C    Humidity: {}% '.format(temperature_f, temperature_c, humidity)
+            current_temp_c = dhtDevice.temperature
+            current_temp_f = current_temp_c * (9 / 5) + 32
+            current_humidity = dhtDevice.humidity
+            log_data = datetime.now().strftime('%d/%m/%Y %H:%M:%S - ') + 'Temp: {:.1f} F / {:.1f} C    Humidity: {}% '.format(current_temp_f, current_temp_c, current_humidity)
             logging.info(log_data)
 
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
-            # print(error.args[0])
+            print(error.args[0])
             time.sleep(2.0)
             continue
         except Exception as error:
@@ -37,9 +53,9 @@ def tempcheck(x):
             raise error
 
         time.sleep(2.0)
-
-        if i == 5:
-            return
+    if current_temp_f != None and current_temp_c != None and current_humidity != None:
+        queue_publish(current_temp_f, current_temp_c, current_humidity)
+    else:
+        tempcheck(5)
 
 tempcheck(5)
-
